867d893be8427e99691ea09a3e67cd565816aaad
tixvage <mel10h3421@gmail.com>
2022-08-31 21:16:10 +0300

    changed resouliton

--- src/vesa.h
+++ src/vesa.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-#define VESA_HEIGHT 1080
-#define VESA_WIDTH 1920
+#define VESA_HEIGHT 768
+#define VESA_WIDTH 1024
 
 void init_vesa_graphics(uint32_t* multiboot_structure);
 uint32_t* get_vesa_fb(void);
